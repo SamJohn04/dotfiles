01db53bda001bd0967a367bb0f9a6178a9904ec3
Samuel John <samuel.john.codes@gmail.com>
2026-08-31 14:59:17 +0530

    update with more dotfiles

---
+++ hypr/config/colors.lua
@@ -0,0 +1,8 @@
+local colors = {
+  primary = "rgb(cba6f7)",
+  surface = "rgb(161616)",
+  secondary = "rgb(7fdbd0)",
+  error = "rgb(b2435d)",
+}
+
+return colors
---
+++ hypr/config/constants.lua
@@ -0,0 +1,10 @@
+local terminal = "kitty"
+
+local constants = {
+  terminal    = terminal,
+  fileManager = terminal .. " yazi",
+  menu        = "wofi",
+  browser     = "librewolf",
+}
+
+return constants
